f8821056918a12bdac349e9a431079f46606ecca
Evan Sosenko <razorx@evansosenko.com>
2016-02-21 14:47:46 -0800

    Add bespoke-hash

--- app/modules/deck.js
+++ app/modules/deck.js
@@ -1,10 +1,12 @@
 import bespoke from 'bespoke'
 import classes from 'bespoke-classes'
+import hash from 'bespoke-hash'
 import keys from 'bespoke-keys'
 import touch from 'bespoke-touch'
 
 const plugins = [
   classes(),
+  hash(),
   keys(),
   touch()
 ]
